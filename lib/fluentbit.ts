a15640270aa2eb1bd63958e27d5e97e18f4d94e9
woohwan <whpark70@naver.com>
2022-12-21 13:34:04 +0900

    opensearch deploy

--- lib/fluentbit.ts
+++ lib/fluentbit.ts
@@ -1,10 +1,30 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as blueprints from "@aws-quickstart/eks-blueprints";
 
-export default class FluentBit extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id);
+const DomainEndpoint = cdk.Fn.importValue("search-test-domain-endpoint");
+const DomainArn = cdk.Fn.importValue("search-test-domain-arn");
 
-    const domain = props?.stackName;
-  }
-}
+const domainWritePolicy = new iam.PolicyStatement({
+  actions: [
+    "es:ESHttpDelete",
+    "es:ESHttpPost",
+    "es:ESHttpPut",
+    "es:ESHttpPatch",
+  ],
+  resources: [DomainArn.toString()],
+});
+
+const awsFluentBit = new blueprints.addons.AwsForFluentBitAddOn({
+  iamPolicies: [domainWritePolicy],
+  values: {
+    elasticSearch: {
+      enabled: true,
+      awsRegion: "ap-northeast-2",
+      host: DomainEndpoint.toString(),
+    },
+  },
+});
+
+export default awsFluentBit;
--- lib/search.ts
+++ lib/search.ts
@@ -1,23 +1,56 @@
 import * as cdk from "aws-cdk-lib";
 import { SecretValue } from "aws-cdk-lib";
-import { Domain, EngineVersion } from "aws-cdk-lib/aws-opensearchservice";
+import { IVpc } from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import {
+  Domain,
+  EngineVersion,
+  DomainProps,
+} from "aws-cdk-lib/aws-opensearchservice";
 import { Construct } from "constructs";
 
 export class SearchStack extends cdk.Stack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
-    const devDomain = new Domain(this, "Domain", {
+
+    const domainProps: DomainProps = {
       version: EngineVersion.OPENSEARCH_1_3,
       domainName: "search-test",
       nodeToNodeEncryption: true,
       encryptionAtRest: {
         enabled: true,
       },
+      // Delete Domain when ckd destroy
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      accessPolicies: [
+        new iam.PolicyStatement({
+          principals: [new iam.AnyPrincipal()],
+          actions: ["es:*"],
+          resources: ["*"],
+          effect: iam.Effect.ALLOW,
+          // conditions: {
+          //   IPAddress: {
+          //     "aws:SourceIp": ["121.140.122.206/32"],
+          //   },
+          // },
+        }),
+      ],
       useUnsignedBasicAuth: true,
       fineGrainedAccessControl: {
         masterUserName: "whpark",
         masterUserPassword: SecretValue.unsafePlainText("Open*0906*"),
       },
+    };
+
+    const devDomain = new Domain(this, "Domain", domainProps);
+
+    new cdk.CfnOutput(this, "DomainEndPoint", {
+      value: devDomain.domainEndpoint,
+      exportName: "search-test-domain-endpoint",
+    });
+    new cdk.CfnOutput(this, "DomainArn", {
+      value: devDomain.domainArn,
+      exportName: "search-test-domain-arn",
     });
   }
 }
