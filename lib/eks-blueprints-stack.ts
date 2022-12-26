5b88752acb2b68b7af46ca0124f3b588d22e838d
woohwan <whpark70@naver.com>
2022-12-26 17:45:14 +0900

    amp #1  permission error

--- lib/eks-blueprints-stack.ts
+++ lib/eks-blueprints-stack.ts
@@ -4,6 +4,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as blueprints from "@aws-quickstart/eks-blueprints";
 import FluentBit from "./fluentbit";
+import { DeploymentMode } from "@aws-quickstart/eks-blueprints";
 
 export default class ClusterConstruct extends Construct {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,11 +12,31 @@ export default class ClusterConstruct extends Construct {
 
     const account = props?.env?.account!;
     const region = props?.env?.region!;
+    const addOns: Array<blueprints.ClusterAddOn> = [
+      FluentBit,
+      new blueprints.addons.CertManagerAddOn({
+        installCRDs: true,
+        createNamespace: true,
+      }),
+      new blueprints.addons.AdotCollectorAddOn(),
+      new blueprints.addons.MetricsServerAddOn(),
+      new blueprints.addons.PrometheusNodeExporterAddOn({
+        version: "4.8.1",
+      }),
+      new blueprints.addons.KubeStateMetricsAddOn(),
+      new blueprints.addons.AmpAddOn({
+        prometheusRemoteWriteURL:
+          "https://aps-workspaces.ap-northeast-1.amazonaws.com/workspaces/ws-5f64c7a9-ef1d-41d0-9229-f7adbd8fff6c/api/v1/remote_write",
+        deploymentMode: DeploymentMode.DEPLOYMENT,
+        namespace: "default",
+        name: "adot-collector-amp",
+      }),
+    ];
 
     const blueprint = blueprints.EksBlueprint.builder()
       .account(account)
       .region(region)
-      .addOns(FluentBit)
+      .addOns(...addOns)
       .teams()
       .build(scope, id + "-stack");
   }
